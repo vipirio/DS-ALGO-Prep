2c48ee4d70de4463e8c5a946a724a19a64250fe2
Vipin Gupta <vipin@paralleldots.com>
2021-01-28 01:55:45 +0530

    fixed issue

--- merge_2_sorted_arr/merge_2_sort_arr.cpp
+++ merge_2_sorted_arr/merge_2_sort_arr.cpp
@@ -6,26 +6,25 @@ public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int last;
         if (n==0){
-            for(int i=0;i<n;i++){
-            cout<<nums1[i]<<" ";
-        }
-        
+            for(int i=0;i<m;i++){
+                cout<<nums1[i]<<" ";
+            }  
         }
         else{
-        for (int i=0; i<m;i++){
-           if(nums1[i]>nums2[0]){
-               last = nums1[i];
-               nums1[i]=nums2[0];
-               nums2[0]=last;
-           }
-           sort(nums2.begin(), nums2.end());
-       }
-        for(int i=0;i<n;i++){
-            nums1[i+m]=nums2[i];
-        }
-        for(int i=0;i<n+m;i++){
-            cout<<nums1[i]<<" ";
-        }
+            for (int i=0; i<m;i++){
+                if(nums1[i]>nums2[0]){
+                    last = nums1[i];
+                    nums1[i]=nums2[0];
+                    nums2[0]=last;
+                }
+                sort(nums2.begin(), nums2.end());
+            }
+            for(int i=0;i<n;i++){
+                nums1[i+m] = nums2[i];
+            }
+            for(int i=0;i<n+m;i++){
+                cout<<nums1[i]<<" ";
+            }
         
         }
     }
@@ -37,11 +36,11 @@ int main (){
     cin>>n;
     vector<int> nums1;
     vector<int> nums2;
-    for (i=0;i<n;i++){
+    for (i=0;i<m;i++){
         cin>>el;
         nums1.push_back(el);
     }
-    for (i=0;i<m;i++){
+    for (i=0;i<n;i++){
         cin>>el;
         nums2.push_back(el);
     }
